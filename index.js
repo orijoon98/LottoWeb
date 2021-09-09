5cc105848ea6900bbb59546e588035f36e88d6b5
HyeokJoon Kong <orijoon98@gmail.com>
2021-09-09 23:17:04 +0900

    https 오류 해결하기

--- index.js
+++ index.js
@@ -90,6 +90,9 @@ function setRecent() { // 최근회차 정보 표시
 }
 
 async function updateLotto() {
+    fetch("https://hyeokjoon.com/data/lottodata.php")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://112.169.11.3:30080/data/lottodata.php", true)
     xhr.responseType='json';
